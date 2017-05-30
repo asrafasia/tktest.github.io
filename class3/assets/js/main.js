431aadb932dbb93d88f84cc7da10b80ab2cbe3d6
asrafasia <asrafasiabd@gmail.com>
2017-05-30 18:06:01 +0600

    changed4

--- class3/assets/js/main.js
+++ class3/assets/js/main.js
@@ -2,12 +2,26 @@
 	"use strict";
 
     jQuery(document).ready(function($){
-
-
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
         
-        $('[data-toggle="tooltip"]').tooltip();
+        
+        $('.video-play-btn').magnificPopup({
+            'type' : 'video',
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+//
+//        $(".embed-responsive iframe").addClass("embed-responsive-item");
+//        $(".carousel-inner .item:first-child").addClass("active");
+//        
+//        $('[data-toggle="tooltip"]').tooltip();
 
 
 
