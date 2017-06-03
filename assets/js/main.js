65285e9e3ce6d21e328cb99c5ee361409f140aec
asrafasia <asrafasiabd@gmail.com>
2017-06-03 23:58:59 +0600

    class7mainjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,21 @@
 
     jQuery(document).ready(function($){
 
-
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
+$(".embed-responsive iframe").addClass("embed-responsive-item");
         $(".carousel-inner .item:first-child").addClass("active");
         
         $('[data-toggle="tooltip"]').tooltip();
 
+        $('.staff-list').owlCarousel({
+            items : 4,
+            autoplay : false,
+            loop : true,
+            margin : 30,
+            nav : true,
+            navText : ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+            
+            
+        });
 
 
         
