df261f76ff74f9a5e99fb01a5d199e8f71b4e94f
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-07-23 15:32:58 +0600

    class32

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,7 +83,19 @@ $(".embed-responsive iframe").addClass("embed-responsive-item");
 			type: 'video',
 		});
 
+		$(window).scroll(function() {
+					if ($(this).scrollTop() > 100) {
+						$('.scrollToTop').fadeIn();
+					} else {
+						$('.scrollToTop').fadeOut();
+					}
+				});
 
+
+				$('.scrollToTop').click(function() {
+					$('html, body').animate({ scrollTop: 0 }, 800);
+					return false;
+				});
 		
 
     });
